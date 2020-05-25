dd7edfe275bd4e9c8a2c71b87b33e1c1a5e0eeda
Richard.C <r.conde@live.com>
2020-05-25 15:53:04 -0400

    creating html mark up

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ function addTransactionDOM(transaction){
 
     // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-    
+
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn">x</button>
+    `;
+
 }
 
